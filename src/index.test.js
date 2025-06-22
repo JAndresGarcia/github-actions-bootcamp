8d20db5b8658866e3a1442e55e9a531aee332625
Julián García <jgdev@Julians-MacBook-Pro.local>
2025-06-21 20:47:50 -0500

    fix: use the correct library for testing

--- src/index.test.js
+++ src/index.test.js
@@ -1,20 +1,19 @@
-const { default: test } = require("node:test")
 const operations = require("./index")
 
 test("Sum two numbers working correctly", () => {
     const result = operations.sumTwoNumbers(2,1);
     
-    expect(result, 3);
+    expect(result).toBe(3);
 })
 
 test("Sustract two numbers working correctly", () => {
     const result = operations.sustractTwoNumbers(5,3);
 
-    expect(result, 2);
+    expect(result).toBe(2);
 })
 
 test("Multiply two numbers working correctly", () => {
     const result = operations.multiplyTwoNumbers(10, 10);
 
-    expect(result, 100);
+    expect(result).toBe(100);
 })
